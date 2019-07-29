fad7e4e231ee4cffdbf6faff83c32e6b51898be1
andywindowsmac <adilkhankenzhetaev@gmail.com>
2019-07-29 11:01:26 +0600

    chore: connect with data

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,19 +1,15 @@
-import Link from 'next/link'
 import * as React from 'react'
 
 import Layout from 'components/Layout'
+import RadioStationsContainer from 'containers/RadioStationsContainer'
+import page from 'hocs/page'
 
 const IndexPage: React.FunctionComponent<{}> = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
+    <Layout title="Stations">
+      <RadioStationsContainer />
     </Layout>
   )
 }
 
-export default IndexPage
+export default page(IndexPage)
